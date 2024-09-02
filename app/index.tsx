c98563161d16fa87be6c1ef516fff3cff33df486
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-09-02 17:21:13 +0300

    adding assets

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import { SafeAreaContext } from "react-native-safe-area-context";
+
+const Home = () => {
+    return (
+        <SafeAreaContext>
+            <Text>Home</Text>
+        </SafeAreaContext>
+    )
+}
+export default Home;
