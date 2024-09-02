496acb397612ad6d51b90b1d5c796e2c5d4fe670
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-09-03 00:58:56 +0300

    adding more clerk auth logic

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -5,8 +5,10 @@ import { images, icons } from "@/constants";
 import { View, Text, ScrollView, Image, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 import OAuth from "@/components/OAuth";
+import { useSignUp } from "@clerk/clerk-expo";
 
 const SignUp = () => {
+    const { isLoaded, signUp, setActive } = useSignUp()
     const [form, setForm] = useState({
         name: '',
         email: '',
@@ -14,9 +16,50 @@ const SignUp = () => {
         confirmPassword: '',
     });
 
+    const [verification, setVerification] = useState({
+        state: 'default',
+        error: '',
+        code: ''
+    });
+
     const onSignUpPress = async () => {
-        // Handle sign-up logic here
-    };
+        if (!isLoaded) {
+            return;
+        }
+
+        try {
+            await signUp.create({
+                emailAddress: form.email,
+                password: form.password,
+            })
+
+            await signUp.prepareEmailAddressVerification({ strategy: 'email_code' })
+
+            setVerification({ ...verification, state: 'pending' })
+        } catch (err: any) {
+            console.error(JSON.stringify(err, null, 2))
+        }
+    }
+
+    const onPressVerify = async () => {
+        if (!isLoaded) return;
+
+        try {
+            const completeSignUp = await signUp.attemptEmailAddressVerification({
+                code: verification.code,
+            });
+
+            if (completeSignUp.status === 'complete') {
+
+                await setActive({ session: completeSignUp.createdSessionId })
+                setVerification({ ...verification, state: 'success' })
+            } else {
+                setVerification({ ...verification, state: 'failed', error: 'Verifiction failed.' })
+            }
+        } catch (err: any) {
+            setVerification({ ...verification, state: 'failed', error: err.errors[0].longMessage })
+        }
+    }
 
     return (
         <ScrollView style={styles.scrollView}>
