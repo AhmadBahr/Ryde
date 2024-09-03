4591f4b5442ea071d4c80850c474b264895d4d2d
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-09-03 22:58:10 +0300

    add mock rides

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,16 +1,15 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text } from 'react-native'
+import { useUser } from '@clerk/clerk-expo'
+import { FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Page() {
-    const { user } = useUser()
+    const { user } = useUser();
 
     return (
-        <SafeAreaView>
-            <SignedIn>
-                <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-            </SignedIn>
+        <SafeAreaView className='bg-general-500'>
+            <FlatList
+                data={[]}
+            />
         </SafeAreaView>
     )
 }
