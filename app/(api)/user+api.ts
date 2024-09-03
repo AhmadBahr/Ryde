3a416f9946fbb5e810b0ea70d8a862192c97b476
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-09-03 22:39:22 +0300

    creating helper functions

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,5 +1,22 @@
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(`${process.env.DATABASE_URL}`);
+export async function POST(request: Request) {
+    try {
+        const sql = neon(`${process.env.DATABASE_URL}`);
 
-export async function POST(Request)
+        const { name, email, clerkId } = await request.json();
+
+        if (!name || !email || !clerkId) {
+            return new Response(JSON.stringify({ error: 'Missing required fields' }), { status: 400 });
+        }
+
+        const result = await sql`INSERT INTO users (name, email, "clerkId") VALUES (${name}, ${email}, ${clerkId}) RETURNING *`;
+
+        return new Response(JSON.stringify({ data: result }), { status: 200 });
+
+    } catch (error) {
+        console.error('Database error:', error);
+
+        return new Response(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
+    }
+}
