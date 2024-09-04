b26ad86b1bcd6d0e4d2d3498693177046529515c
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-09-05 01:55:48 +0300

    adding markers to the map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
-import { calculateRegion } from "../lib/map";
-import { useLocation } from "@/store"; 
+import MapView, { Marker , PROVIDER_DEFAULT } from "react-native-maps";
+import { calculateRegion, generateMarkersFromData } from "../lib/map";
+import { useDriverStore, useLocation } from "@/store";
+import { MarkerData } from "@/types/type";
+import { icons } from "@/constants";
+
+const drivers = [
+    {
+        "id": "1",
+        "first_name": "James",
+        "last_name": "Wilson",
+        "profile_image_url": "https://ucarecdn.com/dae59f69-2c1f-48c3-a883-017bcf0f9950/-/preview/1000x666/",
+        "car_image_url": "https://ucarecdn.com/a2dc52b2-8bf7-4e49-9a36-3ffb5229ed02/-/preview/465x466/",
+        "car_seats": 4,
+        "rating": "4.80"
+    },
+    {
+        "id": "2",
+        "first_name": "David",
+        "last_name": "Brown",
+        "profile_image_url": "https://ucarecdn.com/6ea6d83d-ef1a-483f-9106-837a3a5b3f67/-/preview/1000x666/",
+        "car_image_url": "https://ucarecdn.com/a3872f80-c094-409c-82f8-c9ff38429327/-/preview/930x932/",
+        "car_seats": 5,
+        "rating": "4.60"
+    },
+    {
+        "id": "3",
+        "first_name": "Michael",
+        "last_name": "Johnson",
+        "profile_image_url": "https://ucarecdn.com/0330d85c-232e-4c30-bd04-e5e4d0e3d688/-/preview/826x822/",
+        "car_image_url": "https://ucarecdn.com/289764fb-55b6-4427-b1d1-f655987b4a14/-/preview/930x932/",
+        "car_seats": 4,
+        "rating": "4.70"
+    },
+    {
+        "id": "4",
+        "first_name": "Robert",
+        "last_name": "Green",
+        "profile_image_url": "https://ucarecdn.com/fdfc54df-9d24-40f7-b7d3-6f391561c0db/-/preview/626x417/",
+        "car_image_url": "https://ucarecdn.com/b6fb3b55-7676-4ff3-8484-fb115e268d32/-/preview/930x932/",
+        "car_seats": 4,
+        "rating": "4.90"
+    }
+]
 
 const Map = () => {
     const {
@@ -10,7 +51,10 @@ const Map = () => {
         userLatitude,
         destinationLatitude,
         destinationLongitude,
-    } = useLocation();  
+    } = useLocation();
+
+    const { selectedDriver, setDrivers } = useDriverStore();
+    const [markers, setMarkers] = useState<MarkerData[]>([]);
 
     const region = calculateRegion({
         userLongitude,
@@ -19,48 +63,50 @@ const Map = () => {
         destinationLongitude,
     });
 
+    useEffect(() => {
+        if (Array.isArray(drivers) && userLatitude && userLongitude) {
+            const newMarkers = generateMarkersFromData({
+                data: drivers, 
+                userLatitude,
+                userLongitude,
+            });
+            setMarkers(newMarkers);
+            setDrivers(drivers); 
+        }
+    }, [drivers, userLatitude, userLongitude, setDrivers]);
+
     return (
-        <View style={styles.container}>
-            <MapView
-                provider={PROVIDER_DEFAULT}
-                style={styles.map}
-                mapType="mutedStandard"
-                showsPointsOfInterest={false}
-                initialRegion={region}  
-                showsUserLocation={true}
-                userInterfaceStyle="light"
-            />
-            <View style={styles.overlay}>
-                <Text style={styles.overlayText}>Map</Text>
-            </View>
-        </View>
+        <MapView
+            provider={PROVIDER_DEFAULT}
+            style={styles.map}
+            mapType="mutedStandard"
+            showsPointsOfInterest={false}
+            initialRegion={region}
+            showsUserLocation={true}
+            userInterfaceStyle="light"
+        >
+            {markers.map((marker) => (
+                <Marker
+                    key={marker.id}
+                    coordinate={{
+                        latitude: marker.latitude,
+                        longitude: marker.longitude,
+                    }}
+                    title={marker.title}
+                    image={
+                        selectedDriver === marker.id ? icons.selectedMarker : icons.marker
+                    }
+                />
+            ))}
+        </MapView>
     );
 };
 
 export default Map;
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        height: '100%',
-        borderRadius: 20,
-        overflow: 'hidden',
-    },
     map: {
         width: '100%',
         height: '100%',
-    },
-    overlay: {
-        position: 'absolute',
-        top: 10,
-        left: 10,
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        padding: 10,
-        borderRadius: 5,
-        tintColor: 'black'
-    },
-    overlayText: {
-        color: 'white',
-        fontWeight: 'bold',
     },
 });
