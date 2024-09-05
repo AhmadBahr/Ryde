21317b71813ac65eaf50489a4d80be50ab47c9a8
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-09-05 22:08:44 +0300

    making the bottom shared container

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,14 +1,14 @@
 import { View, Text } from 'react-native'
 import { useLocationStore } from '@/store'
 import React from 'react'
+import RideLayout from '@/components/RideLayout'
 
 const findride = () => {
     const { userAddress, destinationAddress, setDestinationLocation, setUserLocation } = useLocationStore()
     return (
-        <View>
-            <Text className='text-2xl'>You are here: {userAddress}</Text>
-            <Text className='text-2xl'>You are going to: {destinationAddress}</Text>
-        </View>
+        <RideLayout>
+            <Text className='text-2xl'>Find Ride {userAddress}</Text>
+        </RideLayout>
     )
 }
 
