463bac32759d921ecef9bc572a7a22abd067ad0b
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-09-06 19:38:40 +0300

    making the payment tab

---
+++ components/Payment.tsx
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import CustomButton from '@/components/CustomButton'
+
+const Payment = () => {
+    const openPaymentSheet = async () => {
+
+    }
+    return (
+        <>
+            <CustomButton
+                title="Confirm Ride"
+                className="my-10"
+                onPress={openPaymentSheet}
+            />
+        </>
+    )
+}
+
+export default Payment
